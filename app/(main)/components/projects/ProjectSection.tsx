f4696f5360e626da6ec1dd641f9fa3fa4f0e3f59
sillalink <sillalink1@gmail.com>
2025-04-19 18:28:52 +0300

    update files

---
+++ app/(main)/components/projects/ProjectSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../Title'
+
+const ProjectSection = () => {
+  return (
+    <div>
+      <Title title1='proj' title2='ects'/>
+    </div>
+  )
+}
+
+export default ProjectSection
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,14 +1,19 @@
 "use client"
+import Image from 'next/image';
 import AboutSection from './components/about/AboutSection';
 import HeroSection from './components/home/HeroSection';
 import ServicesSection from './components/services/ServicesSection';
+import layer from "../../public/assets/layer1.svg"
 
 export default function HomePage() {
   return (
-    <>
+    <div className='w-full relative'>
       <HeroSection />
       <AboutSection />
       <ServicesSection />
-    </>
+      <div>
+        <Image src={layer} className='w-60 absolute opacity-50' alt='layer'/>
+      </div>
+    </div>
   )
 }
