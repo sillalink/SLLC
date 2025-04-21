ea4af3c43fa7e28fb74d8bfe9d50c724d2d25e2b
Mohammed <hyperlink.hlpc@gmail.com>
2025-04-21 18:20:30 +0300

    update files

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['assets.aceternity.com'],
+    domains: ['assets.aceternity.com' , "sillalink.com" , "www.sillalink.com"],
   },
 };
 
